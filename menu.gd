978903516824b19566d518c8f366bbfae866ca3f
Cookie <yumzuko@gmail.com>
2023-11-13 15:26:11 +0100

    reussi !

--- menu.gd
+++ menu.gd
@@ -1,14 +1,21 @@
 extends Control
 
 var card_scene = preload("res://Things/card.tscn")
-@onready var file = 'res://datas/datas.txt'
+@onready var file = 'res://datas/datas.json'
 
 var longestId = 0
 
-func load():
-	var files = FileAccess.open("datas/datas.json",FileAccess.READ)
-	var datasssss = JSON.parse_string(file.get_as_text())
-	return datasssss
+func load_json_file(filepath: String):
+	if FileAccess.file_exists(filepath):
+		var dataFile = FileAccess.open(filepath,FileAccess.READ)
+		var parsedResult = JSON.parse_string(dataFile.get_as_text())
+		if parsedResult is Dictionary:
+			return parsedResult
+		else:
+			print("Error reading file")
+		dataFile.close()
+	else:
+		print("File doesn't exist !")
 
 func _ready(): #Chercher un système d'id
 	#for i in range(10):
@@ -17,6 +24,78 @@ func _ready(): #Chercher un système d'id
 	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/3", i+1000)
 	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/4", i+10000)
 	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/5", i+100000)
+	var datas = load_json_file(file)
+	global.id_clicked = null
+	var index = 0
+	index = datas.size()
+	
+	longestId = index - 1
+	global.lastId = longestId
+	var count = 0 
+	while count < index:
+		#tant qu'il a pas un id il le fait, pour eviter que ça fasse des troues dans les notes
+		if datas.has("note" + str(count)):
+			create_card_json($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/1",count,datas["note" + str(count)])
+		count +=1
+		if datas.has("note" + str(count)):
+			create_card_json($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/2",count,datas["note" + str(count)])
+		count +=1
+		if datas.has("note" + str(count)):
+			create_card_json($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/3",count,datas["note" + str(count)])
+		count +=1
+		if datas.has("note" + str(count)):
+			create_card_json($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/4",count,datas["note" + str(count)])
+		count +=1
+		if datas.has("note" + str(count)):
+			create_card_json($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/5",count,datas["note" + str(count)])
+		count += 1
+
+func create_card_json(object , count ,data):
+	var card = card_scene.instantiate()
+	card.text = data["text"]
+	card.date = "10 Nov, 2023"
+	card.id = count
+	object.add_child(card)
+
+
+
+
+
+
+
+
+
+
+func create_card(object , i):
+	var card = card_scene.instantiate()
+	
+	var line = ""
+	var f = FileAccess.open("datas/datas.txt",FileAccess.READ)
+	var index = 0
+	while not f.eof_reached(): # iterate through all lines until the end of file is reached
+		if index == i:
+			line = f.get_line()
+			break;
+		index+=1
+		f.get_line()
+	f.close()
+	if !line:
+		line = "Error/NoData"
+
+	card.text = line
+	card.date = "10 Nov, 2023"
+	card.id = i
+	object.add_child(card)
+
+
+func ancien_ready(): #Chercher un système d'id
+	#for i in range(10):
+	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/1", i)
+	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/2", i+100)
+	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/3", i+1000)
+	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/4", i+10000)
+	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/5", i+100000)
+	
 	global.id_clicked = null
 	var f = FileAccess.open("datas/datas.txt",FileAccess.READ)
 	var index = 0
@@ -45,40 +124,3 @@ func _ready(): #Chercher un système d'id
 		if count < longestId:
 			create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/5",count)
 		count += 1
-
-
-func create_card(object , i):
-	var card = card_scene.instantiate()
-	
-	var line = ""
-	var f = FileAccess.open("datas/datas.txt",FileAccess.READ)
-	var index = 0
-	while not f.eof_reached(): # iterate through all lines until the end of file is reached
-		if index == i:
-			line = f.get_line()
-			break;
-		index+=1
-		f.get_line()
-	f.close()
-	if !line:
-		line = "Error/NoData"
-
-	card.text = line
-	card.date = "10 Nov, 2023"
-	card.id = i
-	object.add_child(card)
-	
-func create_card_json(object , i):
-	var data = load("datas/datas.json")
-	var card = card_scene.instantiate()
-	var line = ""
-	var index = 0
-	if data["id"] == i:
-		line = data["text"]
-	if !line:
-		line = "Error/NoData"
-
-	card.text = line
-	card.date = "10 Nov, 2023"
-	card.id = i
-	object.add_child(card)
