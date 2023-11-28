5476a4ad8a57aa234b9e285785ea50efa9dac1c5
Cookie <yumzuko@gmail.com>
2023-11-28 10:31:32 +0100

    multiple window, real window

--- menu.gd
+++ menu.gd
@@ -9,9 +9,10 @@ var longestId = 0
 #En mode "Le popup est déja présent"
 
 #Faire aussi un truc de pinned ? Met en haut la note qui est pinned
-
+@onready var _MainWindow: Window = get_window()
 #Does the screen with all the notes 
 func _ready():
+	_MainWindow.gui_embed_subwindows = false # Make subwindows actual system windows <- VERY IMPORTANT
 	#var time = Time.get_datetime_dict_from_system()
 	#print(time["day"]," ",time["month"], " ",time["year"])
 	var datas = global.load_json_file(file)
@@ -78,8 +79,6 @@ func _input(event):
 	if event.is_action_pressed("ui_down"):
 		pass
 
-
-
 func _on_new_note_pressed():
 	var newNoteAnimation = $Pages/Home/VBoxContainer/Header/HBoxContainer/newNote/AnimationPlayer
 	newNoteAnimation.play("animation")
@@ -93,73 +92,3 @@ func _on_help_pressed():
 	if $Popups.get_child_count() <= 0:
 		var shortcuts = shortcuts_scene.instantiate()
 		$Popups.add_child(shortcuts)
-
-
-
-
-
-
-
-
-
-
-func create_card(object , i):
-	var card = card_scene.instantiate()
-	
-	var line = ""
-	var f = FileAccess.open("datas/datas.txt",FileAccess.READ)
-	var index = 0
-	while not f.eof_reached(): # iterate through all lines until the end of file is reached
-		if index == i:
-			line = f.get_line()
-			break;
-		index+=1
-		f.get_line()
-	f.close()
-	if !line:
-		line = "Error/NoData"
-
-	card.text = line
-	card.date = "10 Nov, 2023"
-	card.id = i
-	object.add_child(card)
-
-
-func ancien_ready(): #Chercher un système d'id
-	#for i in range(10):
-	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/1", i)
-	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/2", i+100)
-	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/3", i+1000)
-	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/4", i+10000)
-	#	create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/5", i+100000)
-	
-	global.id_clicked = null
-	var f = FileAccess.open("datas/datas.txt",FileAccess.READ)
-	var index = 0
-	while not f.eof_reached(): # iterate through all lines until the end of file is reached
-		index+=1
-		
-		f.get_line()
-	f.close()
-	var data = load("res://datas/datas.json")
-	longestId = index - 1
-	global.lastId = longestId
-	var count = 0 
-	while count < index:
-		if count < longestId:
-			create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/1",count)
-		count +=1
-		if count < longestId:
-			create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/2",count)
-		count +=1
-		if count < longestId:
-			create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/3",count)
-		count +=1
-		if count < longestId:
-			create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/4",count)
-		count +=1
-		if count < longestId:
-			create_card($"Pages/Home/VBoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/5",count)
-		count += 1
-
-
